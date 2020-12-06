ec63d622961dbabb25bb796986581bb8f1b3a540
Gomandev <gomandev@gmail.com>
2020-12-06 11:45:45 +0100

    first push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd320604176518bd9ee3b897ef88624b4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b57a5b62-8c47-49a4-bf02-d1b7d0bff7c1'
+};
